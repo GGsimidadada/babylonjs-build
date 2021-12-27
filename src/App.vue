fd308c0813dfce981c2abb5a45d703caffd307e0
gaohaoran <370976188@qq.com>
2021-12-27 22:48:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <canvas id="container" ></canvas>
+</template>
+
+<style>
+html,
+body,
+#app,
+#container {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
