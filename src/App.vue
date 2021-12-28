79c0d2d0530da2f8b3bc99d97615065231b73dee
gaohaoran <370976188@qq.com>
2021-12-28 23:18:00 +0800

    创建engine、scene、camera

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+import { onMounted } from '@vue/runtime-core';
+import { ArcRotateCamera, Engine, Scene, Vector3 } from 'babylonjs';
+
+onMounted(() => {
+  const canvas = document.querySelector<HTMLCanvasElement>('#container');
+  const engine = new Engine(canvas);
+  const scene = createScene(engine);
+  engine.runRenderLoop(() => {
+    scene.render();    
+  });
+});
+
+const createScene = (engine: Engine) => {
+  const scene = new Scene(engine);
+  const camera = new ArcRotateCamera('camera', 0, 0, 10, Vector3.Zero(), scene);
+  return scene;
+};
 
 </script>
 
