2a51e67f6a7a1b6b8a61c5d260c0d2a217d905ee
gaohaoran <370976188@qq.com>
2021-12-29 23:56:43 +0800

    创建模型和灯光

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from '@vue/runtime-core';
-import { ArcRotateCamera, Engine, Scene, Vector3 } from 'babylonjs';
+import { ArcRotateCamera, Color3, Engine, MeshBuilder, Scene, Vector3, Color4, DirectionalLight } from 'babylonjs';
 
 onMounted(() => {
   const canvas = document.querySelector<HTMLCanvasElement>('#container');
@@ -12,8 +12,26 @@ onMounted(() => {
 });
 
 const createScene = (engine: Engine) => {
+  /** 创建场景 */
   const scene = new Scene(engine);
+  scene.clearColor = Color4.FromColor3(Color3.Gray());
+  /** end */
+
+  /** 创建相机 */
   const camera = new ArcRotateCamera('camera', 0, 0, 10, Vector3.Zero(), scene);
+  camera.attachControl(true);
+  /** end */
+
+  /** 创建立方体 */
+  const box = MeshBuilder.CreateBox('box', { width: 2, height: 3, depth: 4 }, scene);
+  /** end */
+
+  /** 创建灯光 */
+  const light = new DirectionalLight('light', Vector3.Down(), scene);
+  light.diffuse = Color3.Red();
+  light.specular = Color3.Green();
+  /** end */
+
   return scene;
 };
 
